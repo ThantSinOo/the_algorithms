6abdf5c40b7d4274f605b578011c8007a21e0ce9
Reggae Homer <mgthantsinmm21@gmail.com>
2023-05-12 14:23:02 +0630

    Fizz Buzz Algorithms - solved

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+const fizzbuzz = (n) =>{
+	for (let i = 0; i <= 100; i++){
+		if(i % 3 === 0 && i % 5 === 0){
+			console.log("FizzBuzz",[i]);
+		}else if(i % 3 === 0){
+			console.log("Fizz",[i])
+		}else if(i % 5 === 0){
+			console.log("Buzz",[i]);
+		}else{
+			console.log([i]);
+		}
+	}
+}
+
+fizzbuzz(100);
