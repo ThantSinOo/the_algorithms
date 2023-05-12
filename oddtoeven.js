b10fd919300ae6a1573506369e1b656deb114588
Reggae Homer <mgthantsinmm21@gmail.com>
2023-05-12 15:34:53 +0630

    Odd to Even Converter Algorithms - still need to solve

---
+++ oddtoeven.js
@@ -0,0 +1,17 @@
+const odd_to_even = (arr) =>{
+	let original_even = [];
+	let edited_even = [];
+	for(let i = 0; i < arr.length; i++){
+		// if([i] % 2 === 0){
+		// 	original_even.push([i]);
+		// }else{
+		// 	let temp = [i] + 1;
+		// 	edited_even.push(temp);
+		// }
+		console.log([i].values);
+	}
+	// console.log("Original Even",original_even);
+	// console.log("Edited Even",edited_even);
+}
+
+odd_to_even([22,32,21,54,45,66,88,37]);
