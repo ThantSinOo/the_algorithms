a84d6cfecef43d845ad4ef7a50236e81e837d7fd
Reggae Homer <mgthantsinmm21@gmail.com>
2023-05-16 23:55:33 +0630

    bogosort algorithms - solved(still need to modify)

---
+++ bogosort.js
@@ -0,0 +1,34 @@
+function isSorted (array) {
+  const length = array.length
+  for (let i = 0; i < length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+ * Shuffles the given array randomly in place.
+ */
+function shuffle (array) {
+  for (let i = array.length - 1; i; i--) {
+    const m = Math.floor(Math.random() * i)
+    const n = array[i - 1]
+    array[i - 1] = array[m]
+    array[m] = n
+  }
+}
+
+/**
+ * Implementation of the bogosort algorithm.
+ *
+ * This sorting algorithm randomly rearranges the array until it is sorted.
+ *
+ */
+export function bogoSort (items) {
+  while (!isSorted(items)) {
+    shuffle(items)
+  }
+  return items
+}
