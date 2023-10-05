c1bb40a0d42de60ebf30ee993c62188c22c06a54
Thant Sin Oo <mgthantsinmm21@gmail.com>
2023-10-05 15:08:39 +0630

    add explanation and comment

--- palindromechecker.js
+++ palindromechecker.js
@@ -40,10 +40,37 @@ function isPalindrome(str) {
     return alphanumericChars.includes(char);
   }
   
-  // Example usage:
+  // Example usage to test:
   const testString1 = "koole";
-  console.log(isPalindrome(testString1)); // Output: true
+  console.log(isPalindrome(testString1)); // Output: will be true if the input is palindrome 
+
+// ---------------------End of code ---------------------------------------------------------------------
+
+
+
+//   What is Palindrome and where are they use for 
+
+//   A palindrome checker algorithm is used to determine whether a given string or sequence of characters 
+//   is a palindrome. 
+//   A palindrome is a word, phrase, or sequence of characters 
+//   that reads the same forwards and backward (ignoring spaces, punctuation, and capitalization).
+
+// -------------------------------------------------------------------------------------------------------
+
+//   Explanation of the algorithm:
+
+//     First, we remove all non-alphanumeric characters from the string and convert it to lowercase. 
+//     This step ensures that we ignore spaces, punctuation, and differences in capitalization.
+
+//     Next, we compare the original string with its reverse. 
+//     We achieve this by splitting the string into an array of characters, 
+//     reversing the array, and then joining it back into a string.
+
+//     If the original string is equal to its reverse, 
+//     the function returns true, indicating that the input string is a palindrome. 
+//     Otherwise, it returns false.
+
+// This algorithm is a straightforward way to check for palindromes in JavaScript. 
+// You can modify it as needed for more complex cases or to handle special requirements.
   
-  const testString2 = "uppu";
-  console.log(isPalindrome(testString2)); // Output: false
   
